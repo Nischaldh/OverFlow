e45b7031e8dcb7f25ba395605dcbe5f57c4ca97a
Nischal Dhungana <nischaldhungana10@gmail.com>
2025-06-02 18:12:50 +0545

    Implement Logout and User avatar

--- components/navigation/LeftSideBar.tsx
+++ components/navigation/LeftSideBar.tsx
@@ -1,81 +1,81 @@
-import React from "react";
-import NavLinks from "./navbar/NavLinks";
-import Link from "next/link";
-import { Button } from "../ui/button";
-import ROUTES from "@/constants/routes";
+import { LogOut } from "lucide-react";
 import Image from "next/image";
-import { auth, signOut } from "@/auth";
+import Link from "next/link";
+import React from "react";
 
-const LeftSideBar = async () => {
+import { auth, signOut } from "@/auth";
+import ROUTES from "@/constants/routes";
+
+import NavLinks from "./navbar/NavLinks";
+import { Button } from "../ui/button";
+
+const LeftSidebar = async () => {
   const session = await auth();
+  const userId = session?.user?.id;
+
   return (
-    <section className="custom-scrollbar background-light900_dark200 light-border sticky left-0 top-0 h-screen flex flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
+    <section className="custom-scrollbar background-light900_dark200 light-border sticky left-0 top-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
       <div className="flex flex-1 flex-col gap-6">
-        <NavLinks />
+        <NavLinks userId={userId} />
       </div>
-      {!session ? (
-        <div className="flex flex-col gap-3">
-          <Button
-            className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none"
-            asChild
+
+      <div className="flex flex-col gap-3">
+        {userId ? (
+          <form
+            action={async () => {
+              "use server";
+
+              await signOut();
+            }}
           >
-            <Link href={ROUTES.SIGN_IN}>
-              <Image
-                src={"/icons/account.svg"}
-                alt="Account"
-                width={20}
-                height={20}
-                className="invert-colors lg:hidden"
-              />
-              <span className="primary-text-gradient max-lg:hidden">
-                Sign In
-              </span>
-            </Link>
-          </Button>
-          <Button
-            className="small-medium light-border-2 btn-tertiary text-dark400_ligth900 min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none"
-            asChild
-          >
-            <Link href={ROUTES.SIGN_UP}>
-              <Image
-                src={"/icons/sign-up.svg"}
-                alt="Sign Up"
-                width={20}
-                height={20}
-                className="invert-colors lg:hidden"
-              />
-              <span className="primary-text-gradient max-lg:hidden">
-                Sign Up
-              </span>
-            </Link>
-          </Button>
-        </div>
-      ) : (
-        <form
-          className="flex items-center justify-start bg-transparent gap-4 p-4 "
-          action={async () => {
-            "use server";
-            await signOut();
-          }}
-        >
-          <Button
-            type="submit"
-            variant="ghost"
-            className="base-medium text-left bg-transparent border-0 p-0 text-dark-400 dark:text-light-900 shadow-none hover:bg-transparent"
-          >
-            <Image
-              src="/icons/logout.svg"
-              alt="Logout"
-              width={20}
-              height={20}
-              className={"invert-colors"}
-            />
-            <p className="base-medium max-lg:hidden ml-2.5">Logout</p>
-          </Button>
-        </form>
-      )}
+            <Button
+              type="submit"
+              className="base-medium flex items-center gap-2 bg-transparent px-4 py-3 text-dark300_light900 shadow-none hover:bg-light-800 dark:hover:bg-dark-300"
+            >
+              <LogOut className="size-5 text-black dark:text-white" />
+              <span className="max-lg:hidden">Logout</span>
+            </Button>
+          </form>
+        ) : (
+          <>
+            <Button
+              className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none"
+              asChild
+            >
+              <Link href={ROUTES.SIGN_IN}>
+                <Image
+                  src="/icons/account.svg"
+                  alt="Account"
+                  width={20}
+                  height={20}
+                  className="invert-colors lg:hidden"
+                />
+                <span className="primary-text-gradient max-lg:hidden">
+                  Log In
+                </span>
+              </Link>
+            </Button>
+
+            <Button
+              className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none"
+              asChild
+            >
+              <Link href={ROUTES.SIGN_UP}>
+                <Image
+                  src="/icons/sign-up.svg"
+                  alt="Account"
+                  width={20}
+                  height={20}
+                  className="invert-colors lg:hidden"
+                />
+                <span className="max-lg:hidden">Sign Up</span>
+              </Link>
+            </Button>
+          </>
+        )}
+      </div>
     </section>
   );
 };
 
-export default LeftSideBar;
+export default LeftSidebar;
--- components/navigation/navbar/NavLinks.tsx
+++ components/navigation/navbar/NavLinks.tsx
@@ -1,45 +1,68 @@
-"use client"
-import { SheetClose } from '@/components/ui/sheet'
-import { sidebarLinks } from '@/constants'
-import { cn } from '@/lib/utils'
-import Image from 'next/image'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-import React from 'react'
+"use client";
+import { SheetClose } from "@/components/ui/sheet";
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
 
-const NavLinks = ({isMobileNav = false}:{isMobileNav?:boolean}) => {
-    const pathname = usePathname();
-    const userId = 1;
-    return(
+const NavLinks = ({
+  isMobileNav = false,
+  userId,
+}: {
+  isMobileNav?: boolean;
+  userId?: string;
+}) => {
+  const pathname = usePathname();
+  return (
     <>
-    {sidebarLinks.map((item)=>{
-        const isActive = (pathname.includes(item.route) && item.route.length>1) || pathname === item.route;
-        if(item.route === '/profile'){
-            if(userId) item.route = `/profile/${userId}`
-            else return null;
+      {sidebarLinks.map((item) => {
+        const isActive =
+          (pathname.includes(item.route) && item.route.length > 1) ||
+          pathname === item.route;
+        if (item.route === "/profile") {
+          if (userId) item.route = `/profile/${userId}`;
+          else return null;
         }
         const LinkComponent = (
-            <Link href={item.route} key = {item.label} className={cn(isActive?'primary-gradient rounded-lg text-ligth-900':'text-dark300_light900',"flex items-center justify-start bg-transparent gap-4 p-4")}>
-                <Image
-                    src={item.imgURL}
-                    alt={item.label}
-                    width={20}
-                    height={20}
-                    className={`invert-colors ${isActive ? "invert-colors" : ""}`}
-                />
-                <p className={cn(isActive?'base-bold':'base-medium',!isMobileNav && "max-lg:hidden")}>{item.label}</p>
-            </Link>
+          <Link
+            href={item.route}
+            key={item.label}
+            className={cn(
+              isActive
+                ? "primary-gradient rounded-lg text-ligth-900"
+                : "text-dark300_light900",
+              "flex items-center justify-start bg-transparent gap-4 p-4  hover:bg-light-800 dark:hover:bg-dark-300"
+            )}
+          >
+            <Image
+              src={item.imgURL}
+              alt={item.label}
+              width={20}
+              height={20}
+              className={`invert-colors ${isActive ? "invert-colors" : ""}`}
+            />
+            <p
+              className={cn(
+                isActive ? "base-bold" : "base-medium",
+                !isMobileNav && "max-lg:hidden"
+              )}
+            >
+              {item.label}
+            </p>
+          </Link>
         );
-        return isMobileNav?(
-            <SheetClose asChild key={item.label}>
-                {LinkComponent}
-            </SheetClose>
-        ):(
-            <React.Fragment key={item.route}>{LinkComponent}</React.Fragment>
-        )
-    })}
+        return isMobileNav ? (
+          <SheetClose asChild key={item.label}>
+            {LinkComponent}
+          </SheetClose>
+        ) : (
+          <React.Fragment key={item.route}>{LinkComponent}</React.Fragment>
+        );
+      })}
     </>
-    )
-}
+  );
+};
 
-export default NavLinks
+export default NavLinks;
